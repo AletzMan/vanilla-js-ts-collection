5aba81da3ea97bd9fc50ff819b7d648e1a012cea
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-20 11:46:35 -0600

    feat: add TypeScript interfaces for project data structure

---
+++ src/data/types.ts
@@ -0,0 +1,17 @@
+export interface Project {
+	name: string;
+	description: string;
+	tags: string[];
+	category: Category;
+	featured: boolean;
+	date: string;
+	difficulty: Difficulty;
+	image: string;
+	language: Language;
+	urlRepo: string;
+	urlDemo: string;
+}
+
+export type Category = "game" | "utility" | "effect" | "component" | "animation";
+export type Language = "javascript" | "typescript";
+export type Difficulty = "easy" | "intermediate" | "advanced";
