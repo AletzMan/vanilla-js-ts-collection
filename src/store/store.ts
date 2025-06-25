6656773fe10cccaa29d85be10a2f9f51bf8c7dd0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-24 23:48:25 -0600

    feat: refactor projectsFiltered to return all projects without filtering logic

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,15 +6,4 @@ export const categorySelected = atom("Todos");
 export const languageSelected = atom("Todos");
 export const difficultySelected = atom("Todos");
 
-export const projectsFiltered = atom<(set: any, get: any) => Project[]>((set, get) => {
-	const category = get(categorySelected);
-	const language = get(languageSelected);
-	const difficulty = get(difficultySelected);
-	return projects.filter((project: Project) => {
-		return (
-			(category === "Todos" || project.category === category) &&
-			(language === "Todos" || project.language === language) &&
-			(difficulty === "Todos" || project.difficulty === difficulty)
-		);
-	});
-});
+export const projectsFiltered = atom<Project[]>(projects);
