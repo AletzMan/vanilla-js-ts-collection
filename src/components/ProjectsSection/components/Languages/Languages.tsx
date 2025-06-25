26be96b31c7c18cf888d2b6c0f72c7e019603c31
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-24 23:48:17 -0600

    feat: add Languages component for project language selection and filtering

---
+++ src/components/ProjectsSection/components/Languages/Languages.tsx
@@ -0,0 +1,47 @@
+import { CheckIcon } from "lucide-react";
+import { projects } from "../../../../data/proyects";
+import { languages } from "../../../../data/filters";
+import styles from "./style.module.css";
+import { useState } from "react";
+import { languageSelected } from "../../../../store/store";
+
+export function Languages() {
+	const [languageSelect, setLanguageSelect] = useState<string[]>([]);
+	const handleLanguageChange = (value: string) => {
+		setLanguageSelect((prev) => {
+			if (prev.includes(value)) {
+				return prev.filter((lang) => lang !== value);
+			} else {
+				return [...prev, value];
+			}
+		});
+		languageSelected.set(value);
+	};
+	return (
+		<div className={styles["language-container"]}>
+			<h2 className={styles.title}>Lenguajes</h2>
+			<div className={styles.container}>
+				{languages.map((language) => (
+					<button
+						key={language.name}
+						onClick={() => handleLanguageChange(language.value)}
+						className={`${styles.language} ${
+							languageSelect.includes(language.value) ? styles["language-checked"] : ""
+						}`}
+					>
+						<div className={styles["language-content"]}>
+							<div className={styles.check}>
+								<CheckIcon className={styles["check-icon"]} />
+							</div>
+							{language.icon && <language.icon className={styles["icon"]} />}
+							{language.name}
+						</div>
+						<span className={styles["count"]}>
+							({projects.filter((project) => project.language === language.value).length})
+						</span>
+					</button>
+				))}
+			</div>
+		</div>
+	);
+}
