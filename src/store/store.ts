d18749b3458db1aaef0d40ba0141c9f571855145
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-26 11:22:39 -0600

    feat: change language and difficulty selection to use arrays for better state management

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,7 @@ import { projects } from "../data/proyects";
 import type { Project } from "../data/types";
 
 export const categorySelected = atom("Todos");
-export const languageSelected = atom("Todos");
-export const difficultySelected = atom("Todos");
+export const languageSelected = atom<string[]>([]);
+export const difficultySelected = atom<string[]>([]);
 
 export const projectsFiltered = atom<Project[]>(projects);
