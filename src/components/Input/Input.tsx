9204ba9e418177ca9bdd39f561039de9a40103f6
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-26 21:18:49 -0600

    feat: refactor Input component to use forwardRef and improve props handling

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,35 +1,27 @@
 import styles from "./styles.module.css";
-import type { JSX } from "react";
+import { forwardRef, type JSX } from "react";
 
-interface Props {
-	type: React.HTMLInputTypeAttribute;
-	placeholder: string;
+interface PropsInput
+	extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size" | "icon" | "onChange"> {
 	size?: "small" | "medium" | "large";
 	icon?: JSX.Element;
 	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-	value?: string;
 }
 
-export default function Input({
-	type,
-	placeholder,
-	size = "medium",
-	icon: Icon,
-	onChange,
-	value,
-}: Props) {
-	return (
-		<div className={styles[`input-container`]}>
-			<input
-				type={type}
-				placeholder={placeholder}
-				className={`${styles.input} ${styles[`input-size-${size}`]} ${
-					Icon ? styles[`input-icon`] : ""
-				}`}
-				value={value}
-				onChange={onChange}
-			/>
-			<span className={styles[`icon`]}>{Icon}</span>
-		</div>
-	);
-}
+export const Input = forwardRef<HTMLInputElement, PropsInput>(
+	({ size, icon, onChange, ...props }, ref) => {
+		return (
+			<div className={styles[`input-container`]}>
+				<input
+					className={`${styles.input} ${styles[`input-size-${size}`]} ${
+						icon ? styles[`input-icon`] : ""
+					}`}
+					ref={ref}
+					onChange={onChange}
+					{...props}
+				/>
+				<span className={styles[`icon`]}>{icon}</span>
+			</div>
+		);
+	}
+);
