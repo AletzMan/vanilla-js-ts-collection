701a3769233ea0c8d60a2508acd5149f2840429c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-06-27 23:59:53 -0600

    feat: enhance Project component with variant support for normal and preview displays

--- src/components/ProjectsSection/components/Project/Project.tsx
+++ src/components/ProjectsSection/components/Project/Project.tsx
@@ -5,28 +5,36 @@ import { Calendar, Github, Link2, StarIcon } from "lucide-react";
 import { categories, difficulties } from "../../../../data/filters";
 import { JavaScriptIcon, TypeScriptIcon } from "../../../../icons";
 
-export default function Project({ project }: { project: Project }) {
+interface Props {
+	project: Project;
+	variant?: "normal" | "preview";
+}
+
+export default function Project({ project, variant = "normal" }: Props) {
 	const IconCategory = categories.find((cat) => cat.value === project.category)?.icon || null;
 	const IconLanguage = project.language === "javascript" ? <JavaScriptIcon /> : <TypeScriptIcon />;
 	const IconDifficulty = difficulties.find((cat) => cat.value === project.difficulty)?.name || "";
 	const IconCategoryColor = categories.find((cat) => cat.value === project.category)?.color || "";
+
 	return (
 		<article className={styles.project} key={project.name}>
 			<header className={styles.header}>
-				<div className={styles["header-info"]}>
-					<span className={styles["header-date"]}>
-						<Calendar />
-						{project.date}
-					</span>
-					<span className={`${styles["header-category"]} `} style={{ color: IconCategoryColor }}>
-						{IconCategory && <IconCategory />}
-					</span>
-				</div>
+				{variant === "normal" && (
+					<div className={styles["header-info"]}>
+						<span className={styles["header-date"]}>
+							<Calendar />
+							{project.date}
+						</span>
+						<span className={styles["header-category"]} style={{ color: IconCategoryColor }}>
+							{IconCategory && <IconCategory />}
+						</span>
+					</div>
+				)}
 				<picture className={styles.picture}>
 					{project.featured && <StarIcon className={styles["featured-icon"]} />}
 					<img src={project.image} alt={project.name} />
 					<a
-						href={`details/${project.id}`}
+						href={`projects/${project.id}`}
 						target="_parent"
 						rel="noopener noreferrer"
 						className={styles["button-details"]}
@@ -35,6 +43,7 @@ export default function Project({ project }: { project: Project }) {
 					</a>
 				</picture>
 			</header>
+
 			<div className={styles.content}>
 				<span className={styles["content-header"]}>
 					<span className={styles["content-id"]}>#{project.id}</span>
@@ -46,36 +55,43 @@ export default function Project({ project }: { project: Project }) {
 						{IconDifficulty}
 					</span>
 				</span>
+
 				<h2>
 					{project.name} {IconLanguage}
 				</h2>
-				{<p>{project.description}</p>}
+
+				{variant === "normal" && <p>{project.description}</p>}
+				{variant === "preview" && <p>{project.description.slice(0, 80)}...</p>}
+
 				<div className={styles.tags}>
 					{project.tags.map((tag) => (
 						<Tag title={tag} size="small" radius="xsmall" key={tag} type="tag" />
 					))}
 				</div>
 			</div>
-			<footer className={styles.footer}>
-				<a
-					href={project.urlRepo}
-					target="_blank"
-					rel="noopener noreferrer"
-					className={styles["footer-link"]}
-				>
-					<Github />
-					Repo
-				</a>
-				<a
-					href={project.urlDemo}
-					target="_blank"
-					rel="noopener noreferrer"
-					className={styles["footer-link"]}
-				>
-					<Link2 />
-					Demo
-				</a>
-			</footer>
+
+			{variant === "normal" && (
+				<footer className={styles.footer}>
+					<a
+						href={project.urlRepo}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={styles["footer-link"]}
+					>
+						<Github />
+						Repo
+					</a>
+					<a
+						href={project.urlDemo}
+						target="_blank"
+						rel="noopener noreferrer"
+						className={styles["footer-link"]}
+					>
+						<Link2 />
+						Demo
+					</a>
+				</footer>
+			)}
 		</article>
 	);
 }
